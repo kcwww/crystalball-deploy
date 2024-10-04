cdd55ac2db0d2290b6a3b65808dfe63ea792933f
kcwww <cwkim0321@gmail.com>
2024-10-04 04:43:08 +0000

    feat: add user header component

--- app/(protected)/main/page.tsx
+++ app/(protected)/main/page.tsx
@@ -1,5 +1,34 @@
+import React from 'react';
+
+import CrystalCanvas from '@/shared/components/canvas/CrystalCanvas';
+import UISection from '@/shared/components/ui/UISection';
+
+import PreviousButton from '@/shared/components/ui/PreviousButton';
+import UserHeader from '@/shared/components/ui/UserHeader';
+import MessageCount from '@/shared/components/ui/MessageCount';
+
+import ShareLink from './_components/ui/ShareLink';
+import FullScreen from './_components/ui/FullScreen';
+import HamburgerButton from './_components/ui/HamburgerButton';
+
 const MainPage = () => {
-  return <div>Main Page</div>;
+  return (
+    <>
+      <PreviousButton />
+      <HamburgerButton />
+      <UISection>
+        <div className="flex flex-col items-center gap-2">
+          <UserHeader user="김부캠" />
+          <MessageCount count={10} />
+        </div>
+        <div className="flex w-full justify-between">
+          <FullScreen />
+          <ShareLink userId="1234" />
+        </div>
+      </UISection>
+      <CrystalCanvas />
+    </>
+  );
 };
 
 export default MainPage;
--- shared/components/ui/UISection.tsx
+++ shared/components/ui/UISection.tsx
@@ -1,24 +1,12 @@
 import ModalProvider from '@/shared/components/providers/ModalProvider';
+import { Toaster } from 'sonner';
 
 const UISection = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section
-      style={{
-        minHeight: '100svh',
-        zIndex: 1,
-        position: 'relative',
-        width: '100%',
-        overflow: 'hidden',
-        pointerEvents: 'none',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: '5rem ',
-      }}
-    >
+    <section className="ui-section">
       {children}
       <ModalProvider />
+      <Toaster />
     </section>
   );
 };
