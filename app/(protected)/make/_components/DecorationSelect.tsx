45c32f6d7c645f907aeecf9b40725fab49985cb6
kcwww <cwkim0321@gmail.com>
2024-11-16 13:35:32 +0000

    fix: suspense error

--- app/(protected)/make/_components/DecorationSelect.tsx
+++ app/(protected)/make/_components/DecorationSelect.tsx
@@ -41,17 +41,17 @@ const DecorationSelect = ({
       onClick={() => handleModel()}
       style={{ width: '18rem', height: '18rem' }}
     >
-      <Canvas style={{ width: '100%', height: '100%' }}>
-        <Suspense fallback={Loading()}>
+      <Suspense fallback={Loading()}>
+        <Canvas style={{ width: '100%', height: '100%' }}>
           <ambientLight intensity={1} color={'#ffffff'} />
           <directionalLight
             position={[10, 20, 10]}
-            intensity={1.5}
+            intensity={0.7}
             color={'#ffffff'}
           />
           <InitializeDecoration path={path} type={type} />
-        </Suspense>
-      </Canvas>
+        </Canvas>
+      </Suspense>
     </DrawerClose>
   );
 };
