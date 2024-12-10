1669d1e3bf2f032f8b3a34fba8aab7a69f61de24
kcwww <cwkim0321@gmail.com>
2024-12-10 07:58:23 +0000

    feat: add landing onboading msg

---
+++ shared/constants/onBoading.ts
@@ -0,0 +1,79 @@
+export const LANDING_ONBOARDING_STEPS = [
+  {
+    target: '.onboarding-crystal-canvas',
+    content:
+      '안녕하세요 수정 구슬 속 내 마음에 오신 것을 환영합니다!\n수정 구슬을 터치하면 수정 구슬 내부에 들어갈 수 있어요 !',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+  {
+    target: '.onboarding-crystal-canvas',
+    content:
+      '추가로 아래 내부장식들을 터치하면 지인들이 남긴 메시지도 볼 수 있어요 !',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+  {
+    target: '.onboarding-service-detail',
+    content: '이곳을 클릭하여 서비스를 좀 더 자세히 알아보세요 !',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+];
+
+export const MAIN_ONBOARDING_STEPS = [
+  {
+    target: '.main-onboarding-crystal-canvas',
+    content:
+      '안녕하세요 ! 방문자들이 남긴 메세지는 수정 구슬 안에 장식으로 보관되어요.',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+  {
+    target: '.main-onboarding-crystal-canvas',
+    content: '추가로 수정 구슬은 총 5개까지만 보유할 수 있습니다.',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+  {
+    target: '.main-onboarding-full-screen',
+    content: '이 버튼을 통해 UI를 숨겨 예쁜 수정 구슬을 감상할 수 있어요.',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+  {
+    target: '.main-onboarding-share-button',
+    content: '이 버튼을 통해 방문자들에게 내 수정구슬 링크를 공유할 수 있어요.',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+  {
+    target: '.main-onboarding-hamburger',
+    content: '추가적으로 필요한 메뉴는 이곳에서 확인할 수 있어요.',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+];
+
+export const VISITOR_ONBOARDING_STEPS = [
+  {
+    target: '.visitor-onboarding-crystal-canvas',
+    content:
+      '안녕하세요 수정 구슬 속 내 마음에 오신 것을 환영합니다!\n수정 구슬을 터치하면 수정 구슬 내부에 들어갈 수 있어요 !',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+  {
+    target: '.visitor-onboarding-crystal-canvas',
+    content:
+      '추가로 아래 내부장식들을 터치하면 다른 방문자들이 남긴 메시지도 볼 수 있어요 !',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+  {
+    target: '.visitor-onboarding-message',
+    content: '이 버튼을 클릭해 새로운 메세지를 선물하세요 !',
+    disableBeacon: true,
+    showBeacon: false,
+  },
+];
